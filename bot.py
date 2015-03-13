262d21f649452b6c4d256a81ce22fab361feda93
Alexander Putilin <avp-13@yandex.ru>
2015-03-13 20:12:33 +0300

    More experimenting, now with comment_stream

--- bot.py
+++ bot.py
@@ -4,17 +4,20 @@ import praw.helpers
 
 reddit_username = os.environ['REDDIT_USERNAME']
 r = praw.Reddit(user_agent='beetlejuicing bot by /u/{}'.format(reddit_username))
-submissions = r.get_subreddit('todayilearned').get_hot(limit=100)
+saved_comments = []
 
 i = 0
-for s in submissions:
-    print "Thread {}...".format(i)
+for c in praw.helpers.comment_stream(r, subreddit='askreddit'):
+    if i % 500 == 0:
+        print "Comment {}...".format(i)
     i += 1
-    for c in praw.helpers.flatten_tree(s.comments):
-        if 'author' not in dir(c):
-            continue
-        # my username godlikeme
-        if ((('God' not in c.body) and ('god' not in c.body) or ('like' not in c.body)) and ('avatar' not in c.body)) or (c.score < 5):
-            continue
-        print "====="
-        print u"{} says: {}".format(c.author, c.body)
+    if 'author' not in dir(c):
+        continue
+    # my username godlikeme
+    if ('god likes' not in c.body) and ('God likes' not in c.body) and ('god hates' not in c.body) and ('God hates' not in c.body):
+        # and ('avatar' not in c.body):
+        continue
+
+    print "====="
+    print u"{} says: {}".format(c.author, c.body)
+    saved_comments.append(c)
